07fa6dbfc7270825fe2bf47a3c974a64a01a22e0
mzeguerrero <mzeguerrero@tip.edu.ph>
2026-04-22 23:55:51 +0800

    fiexed it again

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
           <img src={heroImg} className="base" width="170" height="179" alt="" />
           <img src={reactLogo} className="framework" alt="React logo" />
           <img src={viteLogo} className="vite" alt="Vite logo" />
+          <img src="/profile.jpg" alt="Profile Picture" className="profile-img" />
         </div>
         <div>
           <h1>Get started</h1>
